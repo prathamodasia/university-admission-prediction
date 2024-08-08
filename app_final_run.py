306c3467e4581f660d8f6185b8deb22bacae1211
prathamodasia <modasiapratha@gmail.com>
2024-08-08 12:16:08 +0530

    Update app_final_run.py

--- app_final_run.py
+++ app_final_run.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
+import joblib
 
-clf = pickle.load(open("university.pkl","rb"))
+clf = joblib.load(open("university.pkl","rb"))
 
 def predict(data):
     return clf.predict(data)
